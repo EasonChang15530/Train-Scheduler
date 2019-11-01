2dcf7e99abf222f0fe66139521a2e32810c9f38a
eason <eason15530@gmail.com>
2019-10-31 23:15:34 -0400

    Achieved the collection and display of user input train data, and upload/downloadthem to/from firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,77 @@
-// Capture Button Click
-$(".btn-primary").on("click", function (event) {
-  // prevent form from trying to submit/refresh the page
-  event.preventDefault();
+$(document).ready(function () {
+  // 1. Initialize Firebase
+  var firebaseConfig = {
+    apiKey: "AIzaSyCejodQZy48h23IUmGpJim_Be2X-3t7A9U",
+    authDomain: "bailing-app.firebaseapp.com",
+    databaseURL: "https://bailing-app.firebaseio.com",
+    projectId: "bailing-app",
+    storageBucket: "bailing-app.appspot.com",
+    messagingSenderId: "1045543733221",
+    appId: "1:1045543733221:web:051c2555e16dcf00ee510c"
+  };
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
 
-  // Capture User Inputs and store them into variables
-  var name = $("#employee-name-input").val().trim();
-  var role = $("#role-input").val().trim();
-  var start = $("#start-input").val().trim();
-  var rate = $("#rate-input").val().trim();
-  // var month = 9;
-  // var total = rate * month;
+  // 2. Button for adding trainInfo
+  $("#userSubmit").on("click", function (event) {
+
+    event.preventDefault();
+
+    // Capture User Inputs and store them into variables
+    var getTrainName = $("#trainNameInput").val().trim();
+    var getDestination = $("#destinationInput").val().trim();
+    // This variable is not displayed
+    // This line has question !!!
+    var getFirstTrainTime = moment($("#firstTrainTimeInput").val().trim(), "HH:mm").format("X");
+    var getFrequency = $("#frequencyInput").val().trim();
+
+    // Creates local object for holding train data
+    var trainInfo = {
+      trainName: getTrainName,
+      destination: getDestination,
+      firstTrainTime: getFirstTrainTime,
+      frequency: getFrequency,
+    };
+
+    // Uploads train data to the database
+    database.ref().push(trainInfo);
+
+    // Clears all of the text-boxes
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#firstTrainTimeInput").val("");
+    $("#frequencyInput").val("");
+  });
+
+  // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
+
+    // Store these train data pulled from firebase into variables.
+    var pulledTrainName = childSnapshot.val().trainName;
+    var pulledDestination = childSnapshot.val().destination;
+    var pulledFirstTrainTime = childSnapshot.val().firstTrainTime;
+    var pulledFrequency = childSnapshot.val().frequency;
+
+    // // Prettify the employee start
+    // var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
+    // console.log(empStartPretty);
+    // console.log("----------------------------");
+    // // Calculate the months worked using hardcore math
+    // // To calculate the months worked
+    // var empMonths = moment().diff(moment(empStart, "X"), "months");
+    // console.log(empMonths);
+
+    var newRow = $("<tr>");
+    var trainNameTd = $("<td>").text(pulledTrainName);
+    var destinationTd = $("<td>").text(pulledDestination);
+    var frequencyTd = $("<td>").text(pulledFrequency);
+  
+    newRow.append(trainNameTd, destinationTd, frequencyTd);
+    $("tbody").append(newRow);
+  });
 
 
-  // Console log each of the user inputs to confirm we are receiving them
-  // console.log(name);
-  // console.log(role);
-  // console.log(start);
-  // console.log(rate);
-
-  var newRow = $("<tr>");
-
-  var nameTd = $("<td>").text(name);
-  var roleTd = $("<td>").text(role);
-  var startdateTd = $("<td>").text(start);
-  var monthlyrateTd = $("<td>").text(rate);
-  // var monthsworkedTd = $("<td>").text(month);
-  // var totalbilledTd = $("<td>").text(total);
 
 
-  // nameTd.text(name);
-
-  newRow.append(nameTd, roleTd, startdateTd, monthlyrateTd);
-
-  $("tbody").append(newRow);
-
-
-  // // Assumptions
-  // var tFrequency = 3;
-
-  // // Time is 3:30 AM
-  // var firstTime = "03:30";
-
-  // // First Time (pushed back 1 year to make sure it comes before current time)
-  // var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-  // console.log(firstTimeConverted);
-
-  // Current Time
-
-  var dateFormat = "MM/DD/YYYY";
-  var convertedDate = moment(startdateTd, dateFormat);
-  var currentTime = moment();
-  console.log("CURRENT TIME: " + moment(currentTime).format("DD/MM/YYYY"));
-  console.log(convertedDate.format("MM/DD/YY"));
-  console.log(convertedDate.format("MMM Do, YYYY hh:mm:ss"));
-  console.log(convertedDate.format("X"));
-  // console.log(convertedDate.format("X"));
-  // Difference between the times
-  console.log(convertedDate.toNow());
-  console.log("----------------------------------------");
-  var diffTime = moment().diff(moment(convertedDate), "months");
-  console.log("DIFFERENCE IN TIME: " + diffTime);
-
-  // // Time apart (remainder)
-  // var tRemainder = diffTime % tFrequency;
-  // console.log(tRemainder);
-
-  // // Minute Until Train
-  // var tMinutesTillTrain = tFrequency - tRemainder;
-  // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-  // // Next Train
-  // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-  // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 });
